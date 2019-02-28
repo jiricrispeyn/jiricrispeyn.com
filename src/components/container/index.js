007beb2bb8d581098b968d619ba3fcb6dd33c8b7
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-03-01 00:58:43 +0100

    Create container component

---
+++ src/components/container/index.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.25rem;
+  padding-top: 0;
+`;
+
+export default Container;
