f8e72e16b3b8d0c50fbd6a26790a84e6212beebd
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-03-01 01:00:21 +0100

    Remove experimental social styling

--- src/components/social/style.js
+++ src/components/social/style.js
@@ -12,9 +12,5 @@ export const StyledIcon = styled.a`
   width: 3.125rem;
   height: 3.125rem;
   border-radius: 50%;
-  /* color: ${colors.black}; */
-
-  background: -webkit-linear-gradient(#eee, #333);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  color: ${colors.black};
 `;
