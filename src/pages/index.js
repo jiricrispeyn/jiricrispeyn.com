a50b38ed0f0c8c0a4e5c9a841a2e785015461575
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-03-15 22:44:42 +0100

    Remove unneeded Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Hero from '../components/hero';
@@ -8,7 +7,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Hero />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
