2e3045353e3c54ce42a24082786203f338899239
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-03-16 19:14:25 +0100

    Use black links

--- src/components/typography/style.js
+++ src/components/typography/style.js
@@ -1,15 +1,25 @@
 import styled from '@emotion/styled';
-import { darken } from 'polished';
+import colors from '../../helpers/colors';
 
 export const Anchor = styled.a`
-  color: #736ced;
-  border-bottom: 2px solid #736ced;
+  position: relative;
+  color: ${colors.black};
   text-decoration: none;
   transition: all 0.2s cubic-bezier(0.17, 0.67, 0.83, 0.67);
 
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    left: 0;
+    bottom: 0;
+    background-color: ${colors.black};
+  }
+
   &:hover,
   &:focus {
-    color: ${darken(0.2, '#736ced')};
-    border-color: ${darken(0.2, '#736ced')};
+    color: #000;
+    border-color: #000;
   }
 `;
