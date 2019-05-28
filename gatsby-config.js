b538acc3c33c7f57e61884a9bf976c3d99d3738f
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-05-28 20:11:23 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Jiri Crispeyn`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Jiri Crispeyn — a Front End Engineer based in Belgium`,
     author: `@jiricrispeyn`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
